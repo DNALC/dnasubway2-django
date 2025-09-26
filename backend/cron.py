98c9b6e692c620bd3ec4c05459dedb17d8f79de0
MaxW4 <123494792+MaxW4@users.noreply.github.com>
2025-09-26 16:31:00 -0400

    Fix missing import

--- backend/cron.py
+++ backend/cron.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 import os
 import re
-from .models import BasecallingJob, PodFile, NanoporeSequence, UserNanoporeSequence, JobPodFile, DataFolder, NanoporeSequenceFolder, DemuxResult
+from .models import BasecallingJob, PodFile, NanoporeSequence, UserNanoporeSequence, JobPodFile, DataFolder, NanoporeSequenceFolder, DemuxResult, Job
 from .utils import (
     shelve_instance,
     ensure_instance_ready,
