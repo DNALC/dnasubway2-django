454490070ffc3591076562238df3a51c303ededd
danielhjacobs <danielhunterjacobs@gmail.com>
2026-06-12 15:00:28 -0400

    Ad dnew endpoint

--- backend/urls.py
+++ backend/urls.py
@@ -114,6 +114,7 @@ urlpatterns = [
     path("backend/remove_sanger_from_folder", remove_sanger_from_folder, name='remove_sanger_from_folder'),
     path("backend/add_nanopore_to_folder", add_nanopore_to_folder, name='add_nanopore_to_folder'),
     path("backend/remove_nanopore_from_folder", remove_nanopore_from_folder, name='remove_nanopore_from_folder'),
+    path("backend/set_fastp_jobs_primary", set_fastp_jobs_primary, name='set_fastp_jobs_primary'),
 ]
 
 
